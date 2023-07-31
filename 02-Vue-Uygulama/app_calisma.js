3d43193dcc2a4eaeca19288b6aabca5092305b17
Hasan Arı <septwall@gmail.com>
2023-07-31 16:33:34 +0300

    ilk çalışma

---
+++ 02-Vue-Uygulama/app_calisma.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp(
+  {
+    template:'<h2> '
+  }
+);
+  
+app.mount('.app'); //diyezin olması id olduğu içindi class ise . olmalıdır.
+
+/*
+div etiketin arası aslında templetteki html etiketlerinin yerleşeceği yerdir template te budur yani.
+
+*/ 
