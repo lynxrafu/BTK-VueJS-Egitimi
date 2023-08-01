b374f526eb9942feaea30567b18be48038493d9f
Hasan Arı <septwall@gmail.com>
2023-08-01 11:53:25 +0300

    vue-cli
    vue - cli yüklendi ilk proje oluşturuldu

--- 05-V-Bind/app_calisma.js
+++ 05-V-Bind/app_calisma.js
@@ -1,13 +1,13 @@
 const app = Vue.createApp(
   {//ya v bind : stlye = "özellik" şeklinde kullanırız ya da :stlye = "style deyip devam ederiz."
     template:` 
-    <div :style = "style"> Deneme Yazısı</div>
+    <div v-bind:style = "style"> Deneme Yazısı</div>
   
     `,
     data(){//Burdaki divin içindeki style ağaşıda yazılan styleyi algılamıyor çünkü saçma bişey yolluyoruz. 
       //Devamında v-bind kullanmamız gerekiyor
       return { 
-        style :"color:green"
+        style :"color:aqua"
       }
     },
     methods:{
